699f487f352a1d5f4538e4920c4e51a211f707ed
LeeJaeKeun14 <ljkean14@gmail.com>
2021-11-16 21:48:48 +0900

    프로젝트 기초 구조 만들기

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,6 +8,10 @@ def create_app(test_config=None):
         JSON_AS_ASCII = False  # 한글 깨짐 오류
     )
     
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:devpass@127.0.0.1:3306/elice_flask_board"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    app.secret_key = 'asodfajsdofijac'
+    
     @app.route('/')
     def index():
         return render_template('index.html')
