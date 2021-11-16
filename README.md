90a12b6bb2f0d6e17927fdc7ddf7a8a7f4a44659
LeeJaeKeun14 <ljkean14@gmail.com>
2021-11-16 21:48:58 +0900

    프로젝트 기조 만들기

--- README.md
+++ README.md
@@ -17,17 +17,46 @@
 
 ## 프로젝트 구성
 
-- 프로젝트 필수 기능
+- 프로젝트 필요 기능
+  - 로그인
+  - 회원가입
+  - 로그아웃
+  - 메인 페이지
+  - 대여하기
+  - 반납하기
+  - 책 소개
+
 - 프로젝트 사용 기술
+  - Flask
+  - Mysql
+  - Jinja2
 
 - 파이썬 패키지 버전
 
 > python.3.8
->
+> certifi            2021.10.8
+> charset-normalizer 2.0.7
+> click              8.0.3
+> colorama           0.4.4
+> Flask              2.0.2
+> Flask-SQLAlchemy   2.5.1
+> greenlet           1.1.2
+> idna               3.3
+> itsdangerous       2.0.1
+> Jinja2             3.0.3
+> MarkupSafe         2.0.1
+> pip                19.2.3
+> requests           2.26.0
+> setuptools         41.2.0
+> SQLAlchemy         1.4.27
+> urllib3            1.26.7
+> Werkzeug           2.0.2
 
 ***
 
 ## 프로젝트 기능 설명
+
+- 아직 아무 기능 없음
 
 ***
 
@@ -45,6 +74,18 @@
 
 ## 프로젝트 팀원 역활
 
+- 팀장 : 이재근
+  - DB 관리 : 이재근
+  - 로그인 세션 관리 : 이재근
+  - 프론트앤드 관리 : 이재근
+  - 데이터 관리 : 이재근
+
 ***
 
 ## 프로젝트 버전
+
+- 0.0.1
+  - DB models 구성
+    - User, Book, Book_stock, Book_rental, Book_evaluation
+
+***
---
+++ flask_app/models.py
@@ -0,0 +1,97 @@
+from db_connect import db
+from datetime import datetime
+
+# 유저 정보
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String(30), nullable=False, unique=True)
+    user_pw = db.Column(db.String(30), nullable=False)
+    user_name = db.Column(db.String(20), nullable=False)
+    user_nickname = db.Column(db.String(30), nullable=False, unique=True)
+    
+    # 대여_책 외부키
+    bookrental = db.relationship("Book_rental", backref="user")
+    # 책_평가 외부키
+    bookevaluation = db.relationship("Book_evaluation", backref="user")
+    
+    def __init__(self,user_id,user_pw,user_name,user_nickname):
+        self.user_id = user_id
+        self.user_pw = user_pw
+        self.user_name = user_name
+        self.user_nickname = user_nickname
+
+# 책 정보
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = db.Column(db.Integer,  primary_key=True, autoincrement=True)
+    book_name = db.Column(db.String(100), nullable = False, unique=True)
+    book_publisher = db.Column(db.String(40), nullable=False)
+    book_author = db.Column(db.String(30), nullable=False)
+    book_publication_date = db.Column(db.DateTime, nullable=False)
+    book_pages = db.Column(db.Integer, nullable = False)
+    book_isbn = db.Column(db.BigInteger, nullable=False)
+    book_description = db.Column(db.Text(), nullable=False)
+    
+    # 보유 책 외부키
+    bookstock = db.relationship("Book_stock", backref="book")
+    # 책_평가 외부키
+    bookevaluation = db.relationship("Book_evaluation", backref="book")
+    
+    def __init__(self,book_name,book_publisher,book_author,book_publication_date,
+                book_pages, book_isbn, book_description):
+        self.book_name = book_name
+        self.book_publisher = book_publisher
+        self.book_author = book_author
+        self.book_publication_date = book_publication_date
+        self.book_pages = book_pages
+        self.book_isbn = book_isbn
+        self.book_description = book_description
+        
+class Book_stock(db.Model):
+    __tablename__ = 'book_stock'
+    book_serial_number = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    book_name = db.Column(db.String(100), db.ForeignKey("book.book_name"), nullable = False)
+    is_rental = db.Column(db.Boolean, nullable = False)
+
+    # 대여_책 외부키
+    bookrental = db.relationship("Book_rental", backref="book_stock")
+    
+    def __init__(self,book_name,is_rental):
+        self.book_name = book_name
+        self.is_rental = is_rental
+
+class Book_rental(db.Model):
+    __tablename__ = 'book_rental'
+    id = db.Column(db.Integer,  primary_key=True, autoincrement=True)
+    user_nickname = db.Column(db.String(30), db.ForeignKey("user.user_nickname"), 
+                            nullable = False)
+    book_serial_number = db.Column(db.Integer, db.ForeignKey("book_stock.book_serial_number"), 
+                            nullable = False)
+    book_name = db.Column(db.String(40), nullable = False)
+    rental_date = db.Column(db.DateTime, default=datetime.utcnow)
+    return_date = db.Column(db.DateTime)
+    is_return = db.Column(db.Boolean, nullable = False)
+    
+    def __init__(self,user_nickname,book_serial_number,book_name,return_date,is_return):
+        self.user_nickname = user_nickname
+        self.book_serial_number = book_serial_number
+        self.book_name = book_name
+        self.return_date = return_date
+        self.is_return = is_return
+
+class Book_evaluation(db.Model):
+    __tablename__ = 'book_evaluation'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    book_name = db.Column(db.String(100), db.ForeignKey("book.book_name"), nullable = False)
+    user_nickname = db.Column(db.String(30), db.ForeignKey("user.user_nickname"), 
+                            nullable = False)
+    book_evaluation = db.Column(db.Integer, nullable = False)
+    evaluation_contente = db.Column(db.Text())
+    evaluation_time = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __init__(self,book_name,user_nickname,book_evaluation,evaluation_contente):
+        self.book_name = book_name
+        self.user_nickname = user_nickname
+        self.book_evaluation = book_evaluation
+        self.evaluation_contente = evaluation_contente
