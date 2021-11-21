cccdbf3da9c343ce1ed77aaa95f480a93f6bd9bd
LeeJaeKeun14 <ljkean14@gmail.com>
2021-11-21 23:13:00 +0900

    회원가입 기능 추가

--- book_rental/create.py
+++ book_rental/create.py
@@ -4,15 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app(test_config='dev'):
-    # app = Flask(__name__, instance_relative_config=True)
     app = Flask(__name__)
     
+    # config 속성 부여
     from .config import config_by_name
     app.config.from_object(config_by_name[test_config])
 
-    from .src.views import api_auth, api_index
-    app.register_blueprint(api_auth)
-    app.register_blueprint(api_index)
+    # views 
+    from .src.views import api_index, api_auth
+    app.register_blueprint(api_index) #/ 메인화면
+    app.register_blueprint(api_auth) #/auth 로그인 화면
     
     db.init_app(app)
 
--- book_rental/src/views/auth.py
+++ book_rental/src/views/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, session, flash, redirect, url_for
 from flask_bcrypt import Bcrypt
 from book_rental.src.model import User, Book, Book_stock, Book_rental, Book_evaluation
 from book_rental.create import db
+import re
 from . import api_auth
 
 # api_auth = Blueprint("auth", __name__)
@@ -14,40 +15,76 @@ def load_logged_in_user():
     else:
         g.user = db.session.query(User).filter(User.user_id == user_id).first()
 
+# 중복확인 버튼 고민 흔적
+# @api_auth.route('/duplicate_id', methods=('GET', 'POST'))
+# def duplicate_id():
+#     print("중복확인")
+#     flash(message={"중복확인"}, category="danger")
+#     return jsonify({"result":"중복확인"})
+
 @api_auth.route('/signup', methods=('GET', 'POST'))
 def signup():
-    if request.method == 'POST':
-        user_id = request.form.get('user_id', None)
-        user_pw = request.form.get('user_pw', None)
-        user_name = request.form.get('user_name', None)
-        user_nickname = request.form.get('user_nickname', None)
-        
-        message, messageType = None, None
-        
-        ## 아이디 중복 검사 아이디어 고민중...
-        if user_id is None:
-            message, messageType = '아이디가 유효하지 않습니다.', 'danger'
-        elif user_pw is None:
-            message, messageType = '비밀번호가 유효하지 않습니다.', 'danger'
-        else:
-            user = db.session.query(User).filter(User.user_id == user_id).first()
-            if user is not None:
+    if session.get('user_id') is None: # 로그인 여부 확인
+        if request.method == 'POST':
+            user_id = request.form.get('user_id', None)
+            user_pw = request.form.get('user_pw', None)
+            user_pw_check = request.form.get('user_pw_check', None)
+            user_name = request.form.get('user_name', None)
+            user_nickname = request.form.get('user_nickname', None)
+            
+            message, messageType = None, None
+            
+            # 정규표현식
+            email = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            pw_e = re.compile("[a-zA-Z]+")
+            pw_n = re.compile("[0-9]+")
+            pw_s = re.compile("[^a-zA-Z0-9 \t\n\r\f\v]+")
+            name = re.compile("[가-힣a-zA-Z]")
+            
+            ## 아이디 이메일 검사
+            if email.match(user_id) == None:
+                message, messageType = '아이디를 이메일 형식으로 만들어야 합니다.', 'danger'
+            elif len(user_id) > 30:
+                message, messageType = '아이디를 30자 이내로 만들어야 합니다.', 'danger'
+            ## 아이디 중복 검사
+            elif db.session.query(User).filter(User.user_id == user_id).first() is not None:
                 message, messageType = f'{user_id} 계정은 이미 등록된 계정입니다.', 'warning'
+            ## 비밀번호 10자 이상 영문, 숫자, 특수문자 중 2개 사용
+            elif len(user_pw) >= 10 and \
+            ((pw_e.match(user_pw) and pw_n.match(user_pw)) or \
+            (pw_e.match(user_pw) and pw_s.match(user_pw)) or \
+            (pw_n.match(user_pw) and pw_s.match(user_pw))):
+                message, messageType = '비밀번호가 유효하지 않습니다.', 'danger'
+            ## 비밀번호 8자 이상 영문, 숫자, 특수문자 모두 사용
+            elif len(user_pw) >= 10 and pw_e.match(user_pw) and pw_n.match(user_pw) and pw_s.match(user_pw):
+                message, messageType = '비밀번호가 유효하지 않습니다.', 'danger'
+            ## 비밀번호 확인과 비밀번호 같은지 확인
+            elif user_pw != user_pw_check:
+                message, messageType = '비밀번호가 비밀번호확인과 동일하지 않습니다.', 'danger'
+            ## 이름은 한글과 영문으로만 입력 받아야 합니다.
+            elif user_pw is None:
+                # 정규표현식
+                message, messageType = '비밀번호가 유효하지 않습니다.', 'danger'
+            ## 닉네임 중복 확인
+            elif db.session.query(User).filter(User.user_nickname == user_nickname).first() is not None:
+                message, messageType = f'{user_nickname} 닉네임은 이미 등록된 닉네임입니다.', 'warning'
             
-        if message is None:
-            # 유저 테이블에 추가
+            if message is None:
+                # 유저 테이블에 추가
+                # 비밀번호 암호화
+                pw_hash = Bcrypt.generate_password_hash(user_pw)
+                
+                user = User(user_id, pw_hash, user_name, user_nickname)
+                db.session.add(user)
+                db.session.commit()
+                
+                return redirect(url_for('auth.signin'))
+            
+            flash(message=message, category=messageType)
 
-            pw_hash = Bcrypt.generate_password_hash(user_pw)
-            
-            user = User(user_id, pw_hash, user_name, user_nickname)
-            db.session.add(user)
-            db.session.commit()
-            
-            return redirect(url_for('auth.signin'))
-        
-        flash(message=message, category=messageType)
-
-    return render_template('signup.html')
+        return render_template('signup.html')
+    # 로그인 시 메인화면으로 이동
+    return redirect(url_for('index.index'))
 
 
 @api_auth.route('/signin', methods=('GET', 'POST'))
@@ -72,7 +109,7 @@ def signin():
 
             flash(message=message, category=messageType)
         return render_template('signin.html')
-
+    # 로그인 시 메인화면으로 이동
     return redirect(url_for('index.index'))
 
 
