582f90528e4f749062dde84822f01806894b3981
LeeJaeKeun14 <ljkean14@gmail.com>
2021-11-27 19:06:52 +0900

    메인페이지 정렬

--- book_rental/src/views/index.py
+++ book_rental/src/views/index.py
@@ -3,6 +3,7 @@ from flask_bcrypt import Bcrypt
 from book_rental.src.model import User, Book, Book_stock, Book_rental, Book_evaluation
 from book_rental.create import db
 from math import ceil
+import copy
 import json
 from . import api_index
 
@@ -56,11 +57,10 @@ def index():
     }
     return render_template('index.html', data=data)
 
-
 @api_index.route("/main/<page>", methods=('GET', 'POST'))
-def main(page):
-    
-    page = int(page)
+@api_index.route("/main/<page>/<sort>", methods=('GET', 'POST'))
+def main(page = 1, sort = None):
+    page = int(page[0])
     books_count = db.session.query(Book).count()
     
     limit = 8
@@ -89,13 +89,55 @@ def main(page):
     df['book_evaluation'] = df['book_evaluation'].fillna(0)
     df['book_evaluation'] = df['book_evaluation'].astype("int")
     
+    num_1 = num_2 = num_3 = [""]
+    if sort != None:
+        sort = eval(sort)
+        temp = copy.deepcopy(sort)
+        print(temp)
+        print(temp[0])
+        
+        num_1, num_2, num_3 = temp
+        
+    if request.method == 'POST':
+        num_1 = request.form.get("num_1", "").split(":")
+        num_2 = request.form.get("num_2", "").split(":")
+        num_3 = request.form.get("num_3", "").split(":")
+        sort = copy.deepcopy([num_1, num_2, num_3])
+        print(sort)
+    
+    def change_num(num):
+        if num[0] == "이름": num[0] = "book_name"
+        elif num[0] == "평점": num[0] = "book_evaluation"
+        elif num[0] == "시간": num[0] = "book_publication_date"
+        
+        if num[1] == "높은순": num[1] = False
+        elif num[1] == "빠른순": num[1] = False
+        else: num[1] = True
+        return num
+    
+    if num_3 != [""]:
+        num_3 = change_num(num_3)
+        num_2 = change_num(num_2)
+        num_1 = change_num(num_1)
+        df = df.sort_values([num_1[0], num_2[0], num_3[0]], ascending=[num_1[1], num_2[1], num_3[1]])
+    elif num_2 != [""]:
+        num_2 = change_num(num_2)
+        num_1 = change_num(num_1)
+        print([num_2[0], num_1[0]], [num_2[1], num_1[1]])
+        df = df.sort_values([num_1[0], num_2[0]], ascending=[num_1[1], num_2[1]])
+    elif num_1 != [""]:
+        num_1 = change_num(num_1)
+        df = df.sort_values(num_1[0], ascending=num_1[1])
+    
     offset = (page - 1) * limit
     books = df[offset:offset+limit]
     books = json.loads(books.to_json(orient="records"))
 
+    print(sort)
     data = {
         "books": books,
         "page": page,
-        "totalPage": totalPage
+        "totalPage": totalPage,
+        "sort": sort
     }
     return render_template("main.html", data=data)
